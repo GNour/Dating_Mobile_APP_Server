8a6aca9bdc93ef2987625dfccc43bf3d41fb2b05
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-03 16:14:16 +0300

    Fix table name to user_blocks

--- database/migrations/2021_10_02_115506_create_user_blocks_table.php
+++ database/migrations/2021_10_02_115506_create_user_blocks_table.php
@@ -13,7 +13,7 @@ class CreateUserBlocksTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_blocked', function (Blueprint $table) {
+        Schema::create('user_blocks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('from_user_id')->constrained("users")->onDelete("cascade");
             $table->foreignId('to_user_id')->constrained("users")->onDelete("cascade");
